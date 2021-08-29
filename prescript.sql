ba8b91890ec42a62aa867840ca1d6572f328c0d9
Ashish Arya <62978073+Ashish-Arya-CS@users.noreply.github.com>
2021-08-30 01:12:31 +0530

    Create prescript.sql

---
+++ prescript.sql
@@ -0,0 +1,14 @@
+USE master;
+CREATE DATABASE TestDB;
+USE TestDB;
+GO
+
+CREATE TABLE dbo.Assignment  
+   (API varchar(max) NULL,  
+   Description varchar(max) NULL,  
+   Auth varchar(max) NULL,
+HTTPS varchar(max) NULL,
+Cors varchar(max) NULL,
+Link varchar(max) NULL,
+Category varchar(max) NULL)  
+GO 
